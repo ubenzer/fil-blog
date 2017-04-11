014f56ea379b70f3a1656c97a2ccb69642d351d8
Umut Benzer <umutbenzer@gmail.com>
2017-04-11 20:53:16 +0200

    Add Google Analytics and CSS minification features.

--- contentTypes/templateItems/stylus.js
+++ contentTypes/templateItems/stylus.js
@@ -1,3 +1,4 @@
+import CleanCSS from 'clean-css'
 import Promise from 'bluebird'
 import {chokidar$} from '../../utils/chokidar'
 import {fsPromise} from '../../utils/fs'
@@ -5,7 +6,17 @@ import path from 'path'
 import stylus from 'stylus'
 import {templatePath} from '../../config'
 
+const cleanCss = new CleanCSS({
+  inline: ['remote'],
+  level: {
+    2: {
+      all: false,
+      removeDuplicateRules: true
+    }
+  }
+})
 const renderAsync = Promise.promisify(stylus.render)
+cleanCss.minifyAsync = Promise.promisify(cleanCss.minify)
 
 const styls = {
   content: async () => {
@@ -13,7 +24,8 @@ const styls = {
     const strContent = await fsPromise.readFileAsync(p, 'utf8')
 
     return renderAsync(strContent, {filename: p})
-      .then((css) => ({content: css}))
+      .then((css) => cleanCss.minifyAsync(css))
+      .then((output) => ({content: output.styles}))
   },
   contentWatcher$: () => chokidar$(`${templatePath}/**/*.styl`, {ignoreInitial: true})
 }
