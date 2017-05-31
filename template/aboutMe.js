df543f9774791d2b45ba5f1000d8e97e89dfcb8f
Umut Benzer <umutbenzer@gmail.com>
2017-06-01 00:10:31 +0200

    A better way of creating about me page.

--- template/aboutMe.js
+++ template/aboutMe.js
@@ -1,9 +1,10 @@
 import {authorMiniBio, authorName} from '../../config'
 import {MainContainer} from './components/MainContainer'
+import PropTypes from 'prop-types'
 import React from 'react'
 import {urlForStaticAsset} from '../../app/utils/url'
 
-const template = () =>
+const template = ({bodyText}) =>
   <MainContainer bigHeader fullWidth>
     <div className="card introduction-card main-container__items">
       <div className="introduction-card__about_container">
@@ -16,29 +17,15 @@ const template = () =>
         <h2 className="introduction-card__mini-bio">{authorMiniBio}</h2>
       </div>
       <div className="introduction-card__profile_container">
-        <section className="introduction-card__bio">
-          <p>Merhaba! Ben Umut. Berlin'de yaşayan bir mühendisim.</p>
-          <p>Şu an HERE'da çalışıyorum. İnsanları gitmek istedikleri yere ulaştırıyoruz.</p>
-          <p>Deneyimlerimi ve öğrendiklerimi paylaşmayı severim! <b>2007</b>'de ilk yazımı yazdığım kişisel
-            blogum UBenzer.com Türkiye'nin en eski bloglarındandır.</p>
-          <p>Hobi amaçlı olarak açık kaynak projeler yazıyorum. Bu blogun alt
-            yapısında kullanılan fil de bunlardan biri. İsterseniz fil ile siz de kendi blogunuzu
-            oluşturabilirsiniz.
-          </p>
-          <p>Danışmaktan, araştırmaktan ve alışılmış dışında çözümler denemekten çekinmem. Çıkardığım
-            işin kalitesine güvenirim.
-          </p>
-          <p>
-            <strong>Kullanıcı deneyimi</strong> ilgimi çeken alanlardandır. Arayüzün kullanıcı ile arkada çalışan
-            sistem arasında bir köprü olduğuna inanırım. Kullanıcıyı anlamanın önemine inanırım.
-            A/B testin gücünü bilirim.
-          </p>
-          <p>Bir başka hobim de bilgisayar oyununlarıdır. Bu blogda çeşitli oyunlar hakkında birçok yazı mevcut!</p>
-          <p>Gezmeyi ve yeni ülkeler görmeyi severim. Şu ana kadar 10 ülke gezme fırsatım oldu, ve dahası da olacak!</p>
-          <p>Benimle Facebook'tan veya umut [et] benzer [nokta] me adresinden iletişime geçebilirsiniz.</p>
-        </section>
+        <section
+          className="introduction-card__bio"
+          dangerouslySetInnerHTML={{__html: bodyText}}
+        />
       </div>
     </div>
   </MainContainer>
+
+
+template.propTypes = {bodyText: PropTypes.string.isRequired}
 
 export default template
